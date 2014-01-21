ee505f159474d7360bb17ae1bd64d1edda9a981f
Balaji Natarajan <balajin.nsfz@gmail.com>
2014-01-21 16:31:49 +0530

    Set MIBDIRS env-var and fixed absolute php path in files-posix directory script

--- pkg/files-posix/scripts/MonitorSSLCertificateExpiry/MonitorCertificateExpiry.sh
+++ pkg/files-posix/scripts/MonitorSSLCertificateExpiry/MonitorCertificateExpiry.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-../../apache/bin/php -q MonitorCertificateExpiry.php $*
+inst=`grep ^inst /etc/init.d/uptime_httpd | cut -d= -f2`
+MIBDIRS=$inst/mibs
+export MIBDIRS
+
+/usr/local/uptime/apache/bin/php -q MonitorCertificateExpiry.php $*
